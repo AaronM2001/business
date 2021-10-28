e11237ebb0a9da19d78f58963516741a980a5e4b
Aaron Menocal <2001menocal@gmail.com>
2021-10-27 19:42:15 -0600

    javascript

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-console.log("hello"+"5*10");
-console.log("hello"+"world");
-alert("You have to be 18 to enter this site");
-confirm("Are you over 18");
-prompt("How old are you")
-var name= "Aaron"
-var pizzaCount= "5"
-var totalCost= "5$"
-var isHungry= "true"
+ console.log("hello"+"5*10");
+ console.log("hello"+"world");
+ alert("You have to be 18 to enter this site");
+ confirm("Are you over 18");
+ prompt("How old are you")
+ var name= "Aaron"
+ var pizzaCount= "5"
+ var totalCost= "5$"
+ var isHungry= "true"
 var firstname= "Aaron"
 var lastname= "Menocal"
 console.log(firstname)
 alert("My name is"+" "+firstname);
-document.getElementById("demo").innerHTML="My name is"+""+firstname;
+// document.getElementById("demo").innerHTML="My name is"+""+firstname;
 console.log("My name is"+" "+firstname);
 document.write("My name is"+" "+firstname);
 alert("My name is"+" "+firstname);
-console.log(firstName[5]);
-alert("My name is") + "" + firstName + '\n' + "My last name is" + "" + lastName + '\n' +
-    "My last name is" + "" + lastName ;
+console.log(firstname[5]);
+alert("My name is" + "" + firstname + '\n' + "My last name is" + "" + lastname + '\n' +
+    "My last name is" + "" + lastname) ;
+
+var numChil=13;
+var partnername="madi";
+var geolocation="doverdelewer";
+var job="front end develeper";
+alert("you will be a " + job + "and maried to " + partnername + "with " + numChil + "in " + geolocation );
+document.getElementById("demo").innerHTML="My name is" + "" + firstname + '\n' + "My last name is" + "" + lastname + '\n' +
+"My last name is" + "" + lastname;
+
+// Store your birth year in a variable.
+// Store a future year in a variable.
+// Calculate your 2 possible ages for that year based on the stored values.
+// For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
+// Output them to the screen like so: "I will be either NN or NN in YYYY", substituting the values.
+
+var birthyear= 2001;
+var futureyear=2022;
+alert("I will be either 20 or 21 in " + futureyear)
