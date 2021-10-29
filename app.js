dfcd1b5ea2c82ee5ba4bc335aa723a349fa41af5
Aaron Menocal <2001menocal@gmail.com>
2021-10-28 18:53:24 -0600

    team work

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
- console.log("hello"+"5*10");
- console.log("hello"+"world");
- alert("You have to be 18 to enter this site");
- confirm("Are you over 18");
- prompt("How old are you")
- var name= "Aaron"
- var pizzaCount= "5"
- var totalCost= "5$"
- var isHungry= "true"
-var firstname= "Aaron"
-var lastname= "Menocal"
+console.log("hello" + "5*10");
+console.log("hello" + "world");
+alert("You have to be 18 to enter this site");
+confirm("Are you over 18");
+prompt("How old are you")
+var name = "Aaron"
+var pizzaCount = "5"
+var totalCost = "5$"
+var isHungry = "true"
+var firstname = "Aaron"
+var lastname = "Menocal"
 console.log(firstname)
-alert("My name is"+" "+firstname);
+alert("My name is" + " " + firstname);
 // document.getElementById("demo").innerHTML="My name is"+""+firstname;
-console.log("My name is"+" "+firstname);
-document.write("My name is"+" "+firstname);
-alert("My name is"+" "+firstname);
+console.log("My name is" + " " + firstname);
+document.write("My name is" + " " + firstname);
+alert("My name is" + " " + firstname);
 console.log(firstname[5]);
 alert("My name is" + "" + firstname + '\n' + "My last name is" + "" + lastname + '\n' +
-    "My last name is" + "" + lastname) ;
+    "My last name is" + "" + lastname);
 
-var numChil=13;
-var partnername="madi";
-var geolocation="doverdelewer";
-var job="front end develeper";
-alert("you will be a " + job + "and maried to " + partnername + "with " + numChil + "in " + geolocation );
-document.getElementById("demo").innerHTML="My name is" + "" + firstname + '\n' + "My last name is" + "" + lastname + '\n' +
-"My last name is" + "" + lastname;
+var numChil = 13;
+var partnername = "madi";
+var geolocation = "doverdelewer";
+var job = "front end develeper";
+alert("you will be a " + job + "and maried to " + partnername + "with " + numChil + "in " + geolocation);
+document.getElementById("demo").innerHTML = "My name is" + "" + firstname + '\n' + "My last name is" + "" + lastname + '\n' +
+    "My last name is" + "" + lastname;
 
 // Store your birth year in a variable.
 // Store a future year in a variable.
@@ -33,6 +33,79 @@ document.getElementById("demo").innerHTML="My name is" + "" + firstname + '\n' +
 // For example, if you were born in 1988, then in 2026 you'll be either 37 or 38, depending on what month it is in 2026.
 // Output them to the screen like so: "I will be either NN or NN in YYYY", substituting the values.
 
-var birthyear= 2001;
-var futureyear=2022;
+var birthyear = 2001;
+var futureyear = 2022;
 alert("I will be either 20 or 21 in " + futureyear)
+
+
+
+var lol = 42;
+alert("21 + 21 = " + lol)
+alert("43 - 1 = " + lol)
+alert(lol + " * 1 = " + lol)
+
+var F = "Fahrenheit"
+var C = "Celsius"
+alert(F + " = " + C + " * 1.8 + 32 ")
+
+var favfood = "Chimichangas"
+var helth = "False"
+var cal = 32
+alert(favfood + " are my favorite food")
+alert("Are " + favfood + " healthy for you? " + helth)
+alert(favfood + " have " + cal + " calories")
+// // 3 Create an array called egFamily and add "Julia", "James", and your name to the array. Then, print the egFamily to the console using console.log.
+// // Your Code:
+// // 4 Fix the right side expression so it evaluates to true.
+// // "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal"
+// // Your Code:
+// // 5 Use this equation and the variables fahrenheit and celsius to print the Fahrenheit equivalent of 12°C.
+// //  Use the Celsius-to-Fahrenheit formula to set the fahrenheit varible:
+// //F = C x 1.8 + 32
+// //Log the fahrenheit variable to the console.
+// // Your Code:
+// //6 Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+// // Blowing from the west
+alert("Blowing from the west")
+prompt("Fallen leaves gather")
+confirm("In the east.")
+// // Fallen leaves gather
+// // In the east.
+// // Each string should be printed on its own line.
+// // Hint: You will need to use special characters to produce the following output. For a refresher, feel free to review the previous Escaping Strings topic in this lesson.
+// // Your Code: 
+// //7 Define two variables called thingOne and thingTwo and assign them values. Print the values of both variables in one console.log statement using concatenation. For example,
+// // red blue
+// // where "red" is the value of thingOne and "blue" is the value of thingTwo. Don't forget to use semicolon at the end of each statement!
+// // Your Code:
+
+//  //8 Create a variable called fullName and assign it your full name as a string
+// // Create a variable called age and addign it your age.
+// // Create a varaibel called quote and assign your favorite quote as a string. 
+// // Print out the results 
+// // Your Code:
+var fullName = "Aaron Menocal"
+var age = 20
+var quote = "Real eyes realize real lies"
+alert(fullName + age + quote)
+//  //9 Create a variable called bill and assign it the result of 10.25 + 3.99 + 7.15 (don't perform the calculation yourself, let JavaScript do it!). Next, create a variable called tip and assign it the result of multiplying bill by a 15% tip rate. Finally, add the bill and tip together and store it into a variable called total.
+// // Print the total to the JavaScript console.
+// // Hint: 15% in decimal form is written as 0.15_._
+// // TIP: To print out the total with a dollar sign ( $ ) use string concatenation. To round total up by two decimal points use the toFixed() method. To use toFixed() pass it the number of decimal points you want to use. For example, if total equals 3.9860, then total.toFixed(2) would return 3.99.
+// // Your Code:
+// //10 Use your variables and string concatenation to create your own awesome message and store it in an awesomeMessage variable. Finally, print your awesome message to the JavaScript console.
+// // Your Code:
+//  //11 Create 2 variables one called big and one called small. Set the strings to each: "I am Big like a elepant!" "I am Small like a mouse!". Use string methods to make the 
+// //'small' variable lowercase and the 'big' variable capital.
+// //console.log out the results
+// // Your Code:
+var Big="I am Big like a elepant!"
+var small="I am Small like a mouse!"
+alert(Big + " " + small)
+// //12 Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+// // Blowing from the west
+// // Fallen leaves gather
+// // In the east.
+// // Each string should be printed on its own line.
+// // Hint: You will need to use special characters to produce the following output. For a refresher, feel free to review the previous Escaping Strings topic in this lesson.
+// // Your Code: 
